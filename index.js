4b226e22660f7a8c9f6b55e871a5a51306671c52
Puran kumar Gupta <109130056+puran2003gupta@users.noreply.github.com>
2024-11-11 15:46:52 +0530

    initial

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+    }
+});
+
+let currentPage = 1;
+
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+    socket.emit('pageChange', currentPage);
+    socket.on('changePage', (page) => {
+        currentPage = page;
+        io.emit('pageChange', currentPage);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+});
+
+server.listen(5050, () => {
+    console.log('Server is running on port 5000');
+});
